6be5ba0b8b6bfb513af49bab8ea01dbbeacfe71e
TMC42 <randomemail1444>
2022-10-27 21:14:00 -0500

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # OneClickPDF
-Windows menu click conversion for pdf to jpg and back
+## Windows menu click conversion for pdf to jpg and back
+
+[-] PDF to JPG conversion is complete
+[ ] Script to create windows context menu entry
+[ ] Implement JPG to PDF conversion
--- generatekeys.py
+++ generatekeys.py
@@ -8,9 +8,9 @@ KEY_PATH = r"Directory\Background\shell\oneclickPDF"
 def main():
     topkey = winreg.CreateKeyEx(winreg.HKEY_CLASSES_ROOT, KEY_PATH)
     winreg.SetValue(topkey, '', winreg.REG_SZ, '&oneclickPDF')
-    winreg.SetValue(topkey, 'Icon', winreg.REG_SZ, "C:\Program Files\oneclickPDF\OCIcon.png")
+    winreg.SetValue(topkey, 'Icon', winreg.REG_SZ, "C:\Program Files\oneclickPDF\OCIcon.ico")
     commandkey = winreg.CreateKeyEx(topkey, r"command")
-    winreg.SetValue(commandkey,'', winreg.REG_SZ, "C:\Program Files\oneclickPDF\converterApp.py" + "%1")
+    winreg.SetValue(commandkey,'', winreg.REG_SZ, "C:\Python310\python.exe" + "C:\Program Files\oneclickPDF\converterApp.py" + "%1")
 
 if __name__=="__main__":
     main()
