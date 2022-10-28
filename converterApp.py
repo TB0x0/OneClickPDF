3f99aa59291c5379dbb1fa4826f047bc9bcb15a7
tmc42 <tmclean1212@gmail.com>
2022-10-28 14:04:51 -0500

    Reg key generation is complete and working.

--- converterApp.py
+++ converterApp.py
@@ -4,12 +4,13 @@ from pdf2image import convert_from_path as PDFConv
 from pathlib import Path
 import sys
 import os
+import ctypes
 
 userprofile = os.environ['USERPROFILE']
 CURRENT_USER_PATH = os.path.join(userprofile, 'Documents')
 
 def ConvertPDF(pdf_path, output_folder = CURRENT_USER_PATH, poppler_path = r"C:\Program Files\oneclickPDF\poppler-22.04.0\Library\bin", saveas_seed = "PDF2IMG"):
-    print("File saved to: " + CURRENT_USER_PATH)
+    ctypes.windll.user32.MessageBoxW(0, "File saved to: " + CURRENT_USER_PATH, "File Converted to JPG", 1)
     converted_imgs = PDFConv(pdf_path, poppler_path = poppler_path)
     
     for i in range(len(converted_imgs)):
