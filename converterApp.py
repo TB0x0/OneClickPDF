7ee642f26e857c4aab0f762947d885f04d70fd56
tmc42 <tmclean1212@gmail.com>
2022-12-14 10:56:26 -0600

    Added some better comments and the convertimg func

--- converterApp.py
+++ converterApp.py
@@ -10,13 +10,17 @@ userprofile = os.environ['USERPROFILE']
 CURRENT_USER_PATH = os.path.join(userprofile, 'Documents')
 
 def ConvertPDF(pdf_path, output_folder = CURRENT_USER_PATH, poppler_path = r"C:\Program Files\oneclickPDF\poppler-22.04.0\Library\bin", saveas_seed = "PDF2IMG"):
+    # Function to convert PDFs to images
     ctypes.windll.user32.MessageBoxW(0, "File saved to: " + CURRENT_USER_PATH, "File Converted to JPG", 1)
     converted_imgs = PDFConv(pdf_path, poppler_path = poppler_path)
     
+    # This loop converts every page of the PDF into an image
     for i in range(len(converted_imgs)):
         converted_imgs[i].save(str(output_folder) + '\page' + str(i) + saveas_seed + '.jpg', 'JPEG')
         
-
+def ConvertIMG(img_path, output_folder = CURRENT_USER_PATH):
+    # Function to convert images to PDFs
+    return 0
 
 if __name__=="__main__":
     ConvertPDF(sys.argv[1])
